ef2f81bffd2e006e7f84a9ce9dd0e6e2031028a6
rnavarro97 <richard.navarro.1618@gmail.com>
2022-03-16 16:36:00 -0400

    Initial commit

---
+++ app/src/main/java/com/revature/navdrawercompose/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.revature.navdrawercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.revature.navdrawercompose.ui.theme.NavDrawerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavDrawerComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
